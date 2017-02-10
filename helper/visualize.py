ccd1183dd7a1401d890f502474a13803950176dd
Jonathan Roth <roth.jona@gmail.com>
2017-02-10 21:05:47 +0000

    added visualization.py

---
+++ helper/visualize.py
@@ -0,0 +1,80 @@
+
+import matplotlib
+matplotlib.use('PDF')
+
+import matplotlib.pyplot as plt
+
+import skimage.io
+import sklearn.metrics
+
+import numpy as np
+
+def visualize(pred_y, true_x, true_y, out_dir='.', label=''):
+
+    # TODO
+    skimage.io.imsave(out_dir + "img_probmap_boundary_test.png", pred_y[1,:,:,2])
+
+    plt.figure()
+    plt.hist(pred_y[1,:,:,2].flatten())
+    plt.savefig(out_dir + 'hist_probmap_boundary')
+
+    # print some for visual inspection
+    nSamples = 5 # print all?
+
+    for sampleIndex in range(nSamples):
+        nCols = 4
+        figure, axes = plt.subplots(ncols=nCols, nrows=1, figsize=(nCols*5, 5))
+        figure.tight_layout(pad = 1)
+
+        predFig = axes[0]
+        trueFig = axes[1]
+        compFig = axes[2]
+        cmatFig = axes[3]
+        
+        pred_prob_map = pred_y[sampleIndex,:,:,:]
+        pred_prob_map_vec = pred_prob_map.reshape((-1, 3))
+        pred = np.argmax(pred_prob_map, axis=2)
+        
+        true_prob_map = true_y[sampleIndex,:,:,:]
+        true_prob_map_vec = true_prob_map.reshape((-1, 3))
+        true = np.argmax(true_prob_map, axis=2)
+
+        comp = pred != true
+        
+        cmat = sklearn.metrics.confusion_matrix(y_true = true.flatten(), y_pred = pred.flatten(), labels=[0,1,2])
+
+        predFig.imshow(skimage.color.label2rgb(pred, image=true_x[sampleIndex,:,:,0]))
+        trueFig.imshow(skimage.color.label2rgb(true, image=true_x[sampleIndex,:,:,0]))
+        compFig.imshow(skimage.color.label2rgb(comp, image=true_x[sampleIndex,:,:,0]))
+        cmatFig.matshow(cmat, cmap = "cool")
+        
+        predFig.set_title('Prediction')
+        trueFig.set_title('Truth')
+        compFig.set_title('Errors')
+
+        predFig.axis('off')
+        trueFig.axis('off')
+        compFig.axis('off')
+        
+        cmatFig.set_ylabel('truth')
+        cmatFig.set_xlabel('prediction')
+        
+        # annotate share of pred
+        for x in range(3):
+            for y in range(3):
+                cmatFig.annotate(str(np.round(cmat[x,y],2)), xy=(y, x), 
+                        horizontalalignment='center',
+                        verticalalignment='center', fontsize = 15)
+        
+        plt.savefig(out_dir + label + '_' + str(sampleIndex) + '_vis')
+        classNames = ['background', 'interior', 'boundary']
+        f = open(out_dir + '/' + label + '_' + str(sampleIndex) + '.txt', 'w')
+        f.write(sklearn.metrics.classification_report(pred.flatten(), true.flatten(), target_names=classNames) + '\n')
+        f.write('Jaccard: ' +
+                str(sklearn.metrics.jaccard_similarity_score(y_pred = pred.flatten(), y_true = true.flatten())) +
+                '\n')
+        f.write('Cross Entropy: ' +
+                str(sklearn.metrics.log_loss(y_pred = pred_prob_map_vec, y_true = true_prob_map_vec)) +
+               '\n')
+        f.close()
+
