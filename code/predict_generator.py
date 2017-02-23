76f7cbfe9463736387b37bb12e5a582456703863
Jonathan Roth <jr0th@users.noreply.github.com>
2017-02-23 15:57:59 -0500

    add comments

--- code/predict_generator.py
+++ code/predict_generator.py
@@ -12,11 +12,18 @@ data_dir = '/home/jr0th/github/segmentation/data/set03/'
 weights_path = '/home/jr0th/github/segmentation/results/set03/0223_test/checkpoints/checkpoint.hdf5'
 batch_size = 10
 
+# get generator for test data
 [_, _, test_generator, dim1, dim2] = helper.data_provider.data_from_images(data_dir, batch_size=batch_size)
+
+# build model and laod weights
 model = helper.model_builder.get_model_3d_output(dim1, dim2)
 model.load_weights(weights_path)
 
+# get one batch of data from the generator
 (x, y) = next(test_generator)
 
+# get model predictions
 y_pred = model.predict_on_batch(x)
+
+# visualize them
 helper.visualize.visualize(y_pred, x, y, out_dir=out_dir, label=out_label)
