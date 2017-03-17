04dd864f3831b44563e7d601c3b0904f9d182b55
Jonathan Roth <roth.jona@gmail.com>
2017-03-17 21:44:15 +0000

    made custom callback inherit from default TensorBoard callback in order to have all stats in one TensorBoard file

--- code/helper/callbacks.py
+++ code/helper/callbacks.py
@@ -4,38 +4,33 @@ import keras.backend as K
 import tensorflow as tf
 import pandas as pd
 
-class SplitsAndMergesLogger(keras.callbacks.Callback):
+class SplitsAndMergesLogger(keras.callbacks.TensorBoard):
     
-    
-    def __init__(self, log_dir):
-        super().__init__()
-        self.log_dir = log_dir
-        self.tag_splits_and_merges = 'splits_and_merges'
-        print('constructor done')
+    def __init__(self, log_dir='./logs', histogram_freq=0, write_graph=True, write_images=False):
+        super().__init__(log_dir, histogram_freq, write_graph, write_images)
 
+        self.tag_splits_and_merges = 'splits_and_merges'
+
+        
     def set_model(self, model):
-        self.model = model
-        self.writer = tf.summary.FileWriter(self.log_dir)
+        super().set_model(model)
         
         self.result_tensor = tf.placeholder(tf.float16)
         self.summary_tensor = tf.summary.scalar(self.tag_splits_and_merges, self.result_tensor)
         
-        print('set_model done')
     
     def on_epoch_end(self, epoch, logs):
-        # log stuff to TensorBoard
+        super().on_epoch_end(epoch, logs)
         
         # prepare log
-        result = 42 + epoch
-        summary = K.get_session().run(self.summary_tensor, feed_dict = {self.result_tensor : result})
+        result = 42 + epoch # this is complicated later
+        summary = self.sess.run(self.summary_tensor, feed_dict = {self.result_tensor : result})
         
         # write to file
         self.writer.add_summary(summary, global_step = epoch)
         self.writer.flush()
-        
-    def on_train_end(self, _):
-        self.writer.close()
 
+        
 # Watch out. Does not work with custom metrics as Pandas doesn't handle keys that are functions correctly.
 class BatchLogger(keras.callbacks.Callback):
 
